884f9e52741ee388f53e392f4f71809c2548aa00
Christian Heinrichs <christian.heinrichs@mykolab.ch>
2019-07-14 13:53:19 +0200

    - Add SHA384 algorithm - Change #exit syntax to !exit - Rename $mydec variable

--- multihash.sh
+++ multihash.sh
@@ -2,18 +2,20 @@
 
 # Interactive multihash script
 
-# multihash.sh written by netcyphe <netcyphe@openmailbox.org> - 02/05/2014
+# multihash.sh written by Christian Heinrichs <christian.heinrichs@mykolab.ch>
+# Last update 07/14/2019
 
 while true
     do
-        read -p "Enter the file location or #exit to quit: " mydec
+        read -p "Enter the file location or !exit to quit: " fileloc
 
-        if [ $mydec != "#exit" ]
+        if [ $fileloc != "!exit" ]
             then
-                printf "MD5 sum: "; md5sum $mydec;
-                printf "SHA1 sum: "; sha1sum $mydec;
-                printf "SHA256 sum: "; sha256sum $mydec;
-                printf "SHA512 sum: "; sha512sum $mydec; printf "\n";
+                printf "MD5 sum: "; md5sum $fileloc;
+                printf "SHA1 sum: "; sha1sum $fileloc;
+                printf "SHA256 sum: "; sha256sum $fileloc;
+                printf "SHA384 sum: "; sha384sum $fileloc;
+                printf "SHA512 sum: "; sha512sum $fileloc; printf "\n";
 
                 printf "Done.\n"
         else
